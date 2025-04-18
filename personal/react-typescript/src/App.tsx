ff46447ac00f03eb07ec2f13879ce52047e6f9e5
Hsungjin <hsungjin142@gmail.com>
2025-04-18 16:39:36 +0900

    [개인] 타입스크립트 예제

---
+++ personal/react-typescript/src/App.tsx
@@ -0,0 +1,14 @@
+import { Layout } from './layouts/Layout';
+import { Example1 } from './pages/Example1';
+import { Example2 } from './pages/Example2';
+
+function App() {
+  return (
+    <Layout>
+      <Example1 />
+      <Example2 />
+    </Layout>
+  );
+}
+
+export default App;
